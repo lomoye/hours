081a1db48286fc694cdb1bb1544b01c3e71fb288
lomoye <834033206@qq.com>
2017-06-12 21:39:08 +0800

    fixed

---
+++ hours-web/src/main/java/com/lomoye/hours/web/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.lomoye.hours.web.controller;
+
+import com.lomoye.hours.domain.User;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * Created by lomoye on 2017/6/12.
+ * 测试用
+ */
+@Controller
+@Scope(WebApplicationContext.SCOPE_REQUEST)
+@RequestMapping("/user")
+public class HelloController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    @ResponseBody
+    User giveHelloData(HttpServletRequest request) {
+        User user = new User();
+        user.setName("lomoye");
+        user.setAge(18);
+
+        return user;
+    }
+}
