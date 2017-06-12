8a45af06ee55279edcef9f6e17ddb19317c3e6d2
lomoye <834033206@qq.com>
2017-06-12 21:41:59 +0800

    fixed

---
+++ hours-core/src/main/java/com/lomoye/hours/domain/User.java
@@ -0,0 +1,27 @@
+package com.lomoye.hours.domain;
+
+/**
+ * Created by lomoye on 2017/6/12.
+ * 用户
+ */
+public class User {
+    private String name;
+
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
