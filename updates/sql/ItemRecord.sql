9ea7188f0481aac2c094d7d0b1686509856316d4
lomoye <834033206@qq.com>
2017-06-28 17:08:39 +0800

    fixed

--- updates/sql/ItemRecord.sql
+++ updates/sql/ItemRecord.sql
@@ -1,8 +1,34 @@
+DROP TABLE IF EXISTS item_param;
+CREATE TABLE item_param(
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  item_id bigint(20) NOT NULL,
+  `type` varchar(128) NOT NULL,
+  unit varchar(128) NOT NULL,
+  display_name varchar(128) NOT NULL,
+  `index` int(20) NOT NULL,
+  create_time datetime NOT NULL,
+  modify_time datetime NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS item_param_value;
+CREATE TABLE item_param_value(
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  item_param_id bigint(20) NOT NULL,
+  item_id bigint(20) NOT NULL,
+  `value` varchar(128) NOT NULL,
+  create_time datetime NOT NULL,
+  modify_time datetime NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS item_record;
 CREATE TABLE item_record(
-id bigint(20) NOT NULL AUTO_INCREMENT,
-item_id bigint(20),
-day datetime,
-create_time datetime NOT NULL,
-modify_time datetime NOT NULL,
-PRIMARY KEY (id) 
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  item_id bigint(20) NOT NULL,
+  `day` datetime NOT NULL,
+  create_time datetime NOT NULL,
+  modify_time datetime NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
